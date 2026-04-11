d068e696ac4aafc56fa5a163050fea8cc18095f8
Hemaa <hemalathameesala105@gmail.com>
2026-04-11 09:27:00 +0530

    Message

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log(-1);
+// var a = 10;
+// var a = 60;
+// console.log(a); 
+
+// let c = 50;
+// c=87;
+// confirm.log(c);
+
+// const b = 32;
+// console.log(b);
+
+// for(let index =0;index<=5;index++)
+// {
+//     console.log(index);
+// }
+
+//another way.....
+// let num1 =10;
+// let num2="10";
+// if(num1==num2){
+// console.log(-1);
+// }
+// else{
+// console.log(1);
+// }
+
+
+for(let i=1;i<5;i++)
+{
+    for(let j=1;j<i;j++)
+    {
+        console.log("*");
+    }
+    console.log("*");
+}
+
+
+// Arrays
+
+// let a =[100,"Hemaa",34.6,null,true]
+// for(let i = 0;i<a.length;i++)
+// {
+//     console.log(typeof a[i]);
+// }
+
